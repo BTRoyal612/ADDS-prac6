b86f922a233f31ecc0d0cf700c52dc51a28d6fa1
gbhoang <gb.hoang02@gmail.com>
2022-04-28 00:06:47 +0930

    prac 6

---
+++ Individual.h
@@ -0,0 +1,21 @@
+#ifndef INDIVIDUAL
+#define INDIVIDUAL
+
+#include <string>
+
+class Individual {
+    private:
+        std::string binaryString;
+        int length;
+
+    public:
+        Individual(int n_length);
+        Individual(std::string n_binaryString);
+        std::string getString();
+        int getBit(int pos);
+        void flipBit(int pos);
+        int getMaxOnes();
+        int getLength();
+};
+
+#endif
